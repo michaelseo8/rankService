718d58aa372f3832ca8104bf7dcd23e178ad126f
ndevelop <ndevelop@gmail.com>
2020-12-01 20:43:21 +0900

    Foundation Front F/W 적용

--- rankService/urls.py
+++ rankService/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rank/', TemplateView.as_view(template_name="rank/list.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
