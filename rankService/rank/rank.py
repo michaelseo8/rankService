99e23c5c9b97e33adc678162c0786bc1f53c57a2
ndevelop <ndevelop@gmail.com>
2020-12-08 18:59:11 +0900

    1차 기능 완료

---
+++ rankService/rank/rank.py
@@ -0,0 +1,8 @@
+class Rank:
+    def __init__(self, rank, url, imageurl, mallname, title, productid):
+        self.rank = rank
+        self.url = url
+        self.imageurl = imageurl
+        self.mallname = mallname
+        self.title = title
+        self.productid = productid
